1464f23791adba0d5aa23d2ae0ca699afd81ccff
Siva Rajesh J <siva.rajesh@zohocorp.com>
2025-06-03 19:48:30 +0530

    Add files via upload

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+   name: "sharedKit",
+   platforms: [
+     .iOS(.v14),
+   ],
+   products: [
+      .library(name: "sharedKit", targets: ["sharedKit"])
+   ],
+   targets: [
+      .binaryTarget(
+         name: "sharedKit",
+         url: "https://github.com/sivarajesh-11245/Zoho-Recruit-KMP/releases/download/0.1/sharedKit.xcframework.zip",
+         checksum:"b4e38dbc1eeee1d5534a0e3544d11ef82f8f1b3e3206555f67af3cf8793eb786")
+   ]
+)
