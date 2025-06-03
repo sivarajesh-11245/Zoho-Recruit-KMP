e767e3f485c339b272ffc74ac2cad0620288f14f
Siva Rajesh J <siva.rajesh@zohocorp.com>
2025-06-04 01:10:29 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
    targets: [
       .binaryTarget(
          name: "sharedKit",
-         url: "https://github.com/sivarajesh-11245/Zoho-Recruit-KMP/releases/download/v1.0.0/sharedKit.xcframework.zip",
+         url: "https://github.com/sivarajesh-11245/Zoho-Recruit-KMP/releases/download/v0.2.0-alpha/sharedKit.xcframework.zip",
          checksum:"b4e38dbc1eeee1d5534a0e3544d11ef82f8f1b3e3206555f67af3cf8793eb786")
    ]
 )
